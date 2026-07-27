4467c1620d0d376ad3f6b4c3efea091ab34a313d
CHRISTIAN MOURA DOS SANTOS <cmourasiga@gmail.com>
2026-07-27 11:10:27 -0300

    documenta variável de ajuste de fuso

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_ANON_KEY?: string;
   readonly VITE_SUPABASE_SCHEMA?: string;
   readonly VITE_SUPABASE_EVENTS_VIEW?: string;
+  readonly VITE_ADJUST_SOURCE_TIMEZONE?: string;
 }
 
 interface ImportMeta {
